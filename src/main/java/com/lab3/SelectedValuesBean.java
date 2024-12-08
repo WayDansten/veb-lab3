f2dcd53a794f14fea4d792cb0f2025f04e825524
WayDansten <way.dansten@gmail.com>
2024-12-08 20:52:05 +0300

    input done

--- src/main/java/com/lab3/SelectedValuesBean.java
+++ src/main/java/com/lab3/SelectedValuesBean.java
@@ -7,23 +7,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @Named("selectedValuesBean")
 @RequestScoped
 public class SelectedValuesBean {
 
     private double selectedX = 0;
-    @Setter
     private double selectedY = 0;
     private double selectedR = 1;
-
-    public void setSelectedX(double selectedX) {
-        this.selectedX = selectedX;
-        System.out.println(selectedX);
-    }
-
-    public void setSelectedR(double selectedR) {
-        this.selectedR = selectedR;
-        System.out.println(selectedR);
-    }
 }
